6438ee3faa7bd5b1a31e41622980f4d018aec0b9
Seu nome <email@dominio.com>
2021-11-14 09:41:19 -0300

    atu

--- iw/ibge/ibge_js.js
+++ iw/ibge/ibge_js.js
@@ -1,9 +1,11 @@
 const URL_REGIAO = 'https://servicodados.ibge.gov.br/api/v1/localidades/regioes'
 const SELECT_REGIAO = document.getElementById('regiao');
 const URL_ESTADO = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
-const SELECT_ESTADO = document.getElementById('estado')
+const SELECT_ESTADO = document.getElementById('estado');
+const URL_CIDADE = 'https://servicodados.ibge.gov.br/api/v1/localidades/distritos'
+const SELECT_CIDADE = document.getElementById('cidade');
 
-// Aprnder essa estrutura
+// Aprender essa estrutura
 // fetch()
 //   .then()
 //   .then();
@@ -20,6 +22,14 @@ fetch(URL_ESTADO)
   .then( (resposta) => resposta.json() )
   .then( (estados) => {
     estados.forEach((cadaEstado) => {
-      SELECT_ESTADO.innerHTML += `<option>${cadaEstado.nome}</option>`
+      SELECT_ESTADO.innerHTML += `<option>${cadaEstado.nome}</option>`;
+    });
+  });
+
+fetch(URL_CIDADE)
+  .then( (resposta) => resposta.json () )
+  .then( (cidade) => {
+    cidade.forEach((cadaCidade) => {
+      SELECT_CIDADE.innerHTML += `<option>${cadaCidade.nome}</option>`;
     });
   });
